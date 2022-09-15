46c690e61db7beff5f4d8d4d0be50d6ad6c991a1
humaidi <humaidizakaria1998@gmail.com>
2022-09-15 13:46:50 +0700

    Function Like

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Like;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    //
+    public function likeOrUnlike($id)
+    {
+        //ambil data post berdasarkan id
+        $post = Post::find($id);
+
+        // jika tidak ada post berdasarkan id
+        if (!$post) {
+            return response([
+                'message' => 'Post not found.'
+            ], 403);
+        }
+
+        $like = $post->likes()->where('user_id', auth()->user()->id)->first();
+
+        // jika belum di like/akan di  like
+        if (!$like) {
+            Like::create([
+                'post_id' => $id,
+                'user_id' => auth()->user()->id,
+            ]);
+
+            return response([
+                'message' => 'Liked'
+            ]);
+        }
+
+        // dislike
+        $like->delete();
+
+        return response([
+            'messages' => 'Disliked'
+        ]);
+    }
 }
