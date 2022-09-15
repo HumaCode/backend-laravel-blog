6964113264dc81cac3fc9bd65ddced7f57f8edd6
humaidi <humaidizakaria1998@gmail.com>
2022-09-15 11:40:09 +0700

    Routing

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,4 +29,22 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     // user
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+
+    // post blog
+    Route::get('/posts', [PostController::class, 'index']);    // tampil semua blog
+    Route::post('/posts', [PostController::class, 'store']);    // tambah
+    Route::get('/posts/{id}', [PostController::class, 'show']);  // tampil berdasarkan id
+    Route::put('/posts/{id}', [PostController::class, 'update']);     // edit
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']);   // hapus
+
+    // Comment
+    Route::get('/posts/{id}/comments', [CommentController::class, 'index']);    // tampil comment by id
+    Route::post('/posts/{id}/comments', [CommentController::class, 'store']);    // tambah
+    Route::put('/comments/{id}', [CommentController::class, 'update']);     // edit
+    Route::delete('/comments/{id}', [CommentController::class, 'destroy']);   // hapus
+
+    // Like
+    Route::post('/posts/{id}/like', [LikeController::class, 'likeOrUnlike']);    // like
+
 });
