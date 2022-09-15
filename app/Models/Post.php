a819777f90dd701305876cfc0a33e56fe26658e0
humaidi <humaidizakaria1998@gmail.com>
2022-09-15 11:32:42 +0700

    Relasi tabel

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Comment;
+use App\Models\Like;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'body',
+        'user_id',
+        'image',
+    ];
+
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comment()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function like()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
